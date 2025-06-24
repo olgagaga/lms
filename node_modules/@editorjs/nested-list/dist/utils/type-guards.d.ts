ac05764b9302293acfc8ecc47fceca1dd2713b97
Olga Kuzmich <75477960+olgagaga@users.noreply.github.com>
2025-06-24 10:05:48 +0300

    Delete node_modules directory

--- node_modules/@editorjs/nested-list/dist/utils/type-guards.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Type guard to check if a node is an HTMLElement, then we can safely use it as an HTMLElement
- * @param node
- * @returns {node is HTMLElement}
- */
-export declare function isHtmlElement(node: Node): node is HTMLElement;
